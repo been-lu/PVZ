c44b26d7094f9c21b4c187bdcefa435352be6a43
MKTRSS <55990982+MKTRSS@users.noreply.github.com>
2020-12-13 14:15:39 +0800

    Test0

--- src/test/Test0.java
+++ src/test/Test0.java
@@ -1,8 +1,27 @@
 package test;
 
-public class Test0 {
+import frame.gamewindow.GameWindow;
+import frame.mainwindow.MainWindow;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Test0 extends JFrame {
+    public static MainWindow m = new MainWindow();
+    public static Test0 p;
+    public static GameWindow p2;
+
+    public Test0(){
+        this.setSize(900, 630);
+        this.setLocation(0, 0);
+        this.add(m);
+        this.setDefaultCloseOperation(3);
+        this.setResizable(false);
+        this.setLocationRelativeTo((Component)null);
+        this.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("hello world!");
+        p = new Test0();
     }
 }
